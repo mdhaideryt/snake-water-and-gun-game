38c6047c0628a146fc26b7e78023c86324fb92b9
mdhaideryt <110153614+mdhaideryt@users.noreply.github.com>
2022-07-30 13:03:09 +0530

    Add files via upload

---
+++ exercise 6.py
@@ -0,0 +1,56 @@
+#snake water gun
+m=10
+you=0
+cpu=0
+l=("snake","water","gun")
+while(True):
+    if m>0:
+        n = input("press s for snake, w for water and g for gun\n")
+        m=m-1
+        import random
+        c = random.choice(l)
+        if n == "s" and c == "water":
+            print("you are winner")
+            you = you + 1
+            print("remaining ",m,"out of 10\n")
+        elif n == "s":
+            print("you are defeated")
+            cpu = cpu + 1
+            print("remaining ", m, "out of 10\n")
+        elif n == "w" and c == "snake":
+            print("you are defeated")
+            cpu = cpu + 1
+            print("remaining ", m, "out of 10\n")
+        elif n == "w":
+            print("you are winner")
+            you = you + 1
+            print("remaining ", m, "out of 10\n")
+        elif n == "g" and c == "snake":
+            print("you are winner")
+            you = you + 1
+            print("remaining ", m, "out of 10\n")
+        else:
+            print("you are defeated")
+            cpu = cpu + 1
+            print("remaining ", m, "out of 10\n")
+    else:
+        print("tournament over, here are the results\n")
+
+        break
+
+print("cpu won ",cpu,"times")
+print("you won ",you,"times")
+if cpu>you:
+    print("So,you lost in this tournamment")
+elif you>cpu:
+    print("Congratulations :you won in this tournamment")
+else:
+    print("No winner")
+
+
+
+
+
+
+
+
