9cb67f6e19b3bb766bf3dd377f8189418bd5640f
mdhaideryt <110153614+mdhaideryt@users.noreply.github.com>
2022-07-30 15:52:58 +0530

    Update exercise 6.py

--- exercise 6.py
+++ exercise 6.py
@@ -12,32 +12,35 @@ while(True):
         if n == "s" and c == "water":
             print("you are winner")
             you = you + 1
-            print("remaining ",m,"out of 10\n")
+            print("attempt remaining ",m,"out of 10\n")
         elif n == "s":
             print("you are defeated")
             cpu = cpu + 1
-            print("remaining ", m, "out of 10\n")
+            print("attempt remaining ", m, "out of 10\n")
         elif n == "w" and c == "snake":
             print("you are defeated")
             cpu = cpu + 1
-            print("remaining ", m, "out of 10\n")
+            print("attempt remaining ", m, "out of 10\n")
         elif n == "w":
             print("you are winner")
             you = you + 1
-            print("remaining ", m, "out of 10\n")
+            print("attempt remaining ", m, "out of 10\n")
         elif n == "g" and c == "snake":
             print("you are winner")
             you = you + 1
-            print("remaining ", m, "out of 10\n")
-        else:
+            print("attempt remaining ", m, "out of 10\n")
+        elif n=="g":
             print("you are defeated")
             cpu = cpu + 1
-            print("remaining ", m, "out of 10\n")
+            print("attempt remaining ", m, "out of 10\n")
+        else:
+            print("check your input and try again\n")
+            print("attempt remaining ", m, "out of 10\n")
+
     else:
         print("tournament over, here are the results\n")
 
         break
-
 print("cpu won ",cpu,"times")
 print("you won ",you,"times")
 if cpu>you:
@@ -46,11 +49,3 @@ elif you>cpu:
     print("Congratulations :you won in this tournamment")
 else:
     print("No winner")
-
-
-
-
-
-
-
-
